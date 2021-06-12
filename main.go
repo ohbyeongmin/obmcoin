e096ecab9b331be64f03c0e78f5ea7c5f130a89b
ohbyeongmin <fbottlem@gmail.com>
2021-06-12 16:22:26 +0900

    blockchain

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/ohbyeongmin/obmcoin/blockchain"
+)
+
 
 func main(){
-	fmt.Println("It Works!")
+	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Block")
+	chain.AddBlock("Fourth Block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n",block.Data)
+		fmt.Printf("Hash: %s\n",block.Hash)
+		fmt.Printf("Prev Hash: %s\n",block.PrevHash)
+	}
 }
